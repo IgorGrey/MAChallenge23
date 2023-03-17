fed061730c220b8ccb5b2ce9c82af7b85cd2c0a1
domdev <drgn.domdev@gmail.com>
2023-03-17 00:35:35 +0000

    final update

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import subprocess
 import threading
 import pynmea2
 
+# Open powershell and run this script and listening port
+# The same can be done for input if needed
+
 "$CCTHD,25.00, 0.00,0.00,0.00,0.00,20.00,0.00,0.00"
 "$CCTHD"
 
@@ -30,25 +33,28 @@ def setup_input_console(port="COM5"):
     response_thread.start()
 
     while True:
-        new_cmd = input("Enter a command:")
-        if new_cmd:
-            _online_port.write(new_cmd)
+        try:
+            # INPUT FORMAT $COMMAND<NUMBERS>,<NUMBERS>,..<CHECKSUM>
+            # OR SIMPLE WAY $SENTENCE*CHECKSUM
+            new_cmd = input("Enter a command:")
+            new_cmd = f"{new_cmd}\r\n".encode("ascii")
+            if new_cmd:
+                _online_port.write(new_cmd)
+        except:
+            print("Error has occured")
+            continue
         
 
-def setup_listening_console(port="COM6", baudrate=111520):
+def setup_listening_console(port="COM6", baudrate=9600):
     _online_port = ports_module.connect_to_port(port)
     while True:
-        # print(_online_port.readline().decode())
         response = _online_port.readline().decode()
-        # print(response)
+        print(response)
         try:
             nmea_res = pynmea2.parse(response)
         except pynmea2.ParseError as pynmea_res:
-            print(pynmea_res)
             continue
     
-        print(nmea_res.sentence_type)
-            
         if nmea_res.sentence_type == "RMC":
             tkp = nmea_res.talker_id, nmea_res.datestamp, nmea_res.latitude, nmea_res.longitude
 
