4e24a30558de7dad8d9c970da14d674d387e7f36
Igor Kanchukovskyi <mail.igorkan@gmail.com>
2023-03-30 21:54:44 +0100

    Create angle_between_waypoints.py
    This would output the angle between the three waypoints in degrees.

---
+++ angle_between_waypoints.py
@@ -0,0 +1,19 @@
+import math
+
+def angle_between_waypoints(coords):
+    # Convert the coordinates to radians
+    coords = [(math.radians(float(lat)), math.radians(float(lon))) for (lat, lon) in coords]
+    
+    # Calculate the vectors between the waypoints
+    v1 = (coords[0][0] - coords[1][0], coords[0][1] - coords[1][1])
+    v2 = (coords[2][0] - coords[1][0], coords[2][1] - coords[1][1])
+    
+    # Calculate the dot product and the magnitudes of the vectors
+    dot_product = v1[0]*v2[0] + v1[1]*v2[1]
+    mag_v1 = math.sqrt(v1[0]**2 + v1[1]**2)
+    mag_v2 = math.sqrt(v2[0]**2 + v2[1]**2)
+    
+    # Calculate the angle between the vectors
+    angle = math.degrees(math.acos(dot_product / (mag_v1 * mag_v2)))
+    
+    return angle
