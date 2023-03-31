f454745eca270f5ac0cda0da82e6417d3e6e0ed2
domdev <drgn.domdev@gmail.com>
2023-03-31 15:09:45 +0100

    Upload from today

--- headingFormula.py
+++ headingFormula.py
@@ -9,12 +9,15 @@ def calculate_heading(lat2, lon2, lat1, lon1):
 
     # Calculate the difference in longitude
     delta_lon = lon2 - lon1
+    print(lon1, lon2, delta_lon)
 
     # Calculate the heading angle using the formula
     y = math.sin(delta_lon) * math.cos(lat2)
     x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(delta_lon)
     heading = math.atan2(y, x)
     heading = math.degrees(heading)
+    print(heading)
+    print(x,y)
 
     # Normalize the heading angle to a value between 0 and 360 degrees
     if heading < 0:
@@ -23,9 +26,9 @@ def calculate_heading(lat2, lon2, lat1, lon1):
     return heading
 
 # Test the function with the provided GPS coordinates
-#lat2 = 50.7000
-#lon2 = -0.44773
-#lat1 = 50.50710799
-#lon1 = -000.44755897
-#heading = calculate_heading(lat1, lon1, lat2, lon2)
+#lat1 = 50.70000000
+#lon1 = 0.44773000
+#lat2 = 50.50710799
+#lon2 = 0.44755897
+#heading = calculate_heading(lat2, lon2, lat1, lon1)
 #print("Heading: {:.1f} degrees".format(heading))
