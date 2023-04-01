3fb6be23d27247b353ab5915db47a25cfc87813c
Igor Kanchukovskyi <mail.igorkan@gmail.com>
2023-04-02 00:33:36 +0100

    Update headingStandalone.py
    added extract lat lon from wpl function, updated rounding to 7  decimal placesin convert to dd function and updated return statment in extract from rmc fun to return float 6 decimal places

--- headingStandalone.py
+++ headingStandalone.py
@@ -7,7 +7,8 @@ def convert_gps_format(gps_format):
     """
     degrees = float(gps_format[:2])
     minutes = float(gps_format[2:])
-    return degrees + (minutes / 60)
+    decimal_degrees = degrees + (minutes / 60.0)
+    return round(decimal_degrees, 7)
 
 def extract_lat_lon(nmea_sentence):
     """
@@ -24,7 +25,24 @@ def extract_lat_lon(nmea_sentence):
         lon = convert_gps_format(data[5])
         if data[6] == "W":
             lon = -lon
-    return lat, lon
+    return round(lat, 6), round(lon, 6)
+
+def extract_lat_lon_from_wpl(nmea_sentence):
+    """
+    Extracts latitude and longitude from a MMWPL NMEA sentence.
+    Example: $MMWPL,5050.710799,N,00044.755897,W,WPT 1*73
+    """
+    lat = 0.0
+    lon = 0.0
+    if nmea_sentence.find("MMWPL") > 0:
+        data = nmea_sentence.split(",")
+        lat = convert_gps_format(data[1])
+        if data[2] == "S":
+            lat = -lat
+        lon = convert_gps_format(data[3])
+        if data[4] == "W":
+            lon = -lon
+    return round(lat, 6), round(lon, 6)
 
 def calculate_heading(current_lat, current_lon, target_lat, target_lon):
     """
@@ -48,20 +66,10 @@ def calculate_heading(current_lat, current_lon, target_lat, target_lon):
     
     return heading_deg
 
-def ddm_to_dd(latitude, longitude):
-    lat_degrees = int(latitude)
-    lat_decimal_minutes = latitude - lat_degrees
-    lat_dd = lat_degrees + (lat_decimal_minutes * 60) / 3600
-
-    lon_degrees = int(longitude)
-    lon_decimal_minutes = longitude - lon_degrees
-    lon_dd = lon_degrees + (lon_decimal_minutes * 60) / 3600
-
-    return lat_dd, lon_dd
-
 # Example usage
-nmea_sentence = "$GPRMC,001115.81,A,5050.710799,N,00044.755897,W,0.0,269.7,230418,4.0,W,A,S*43"
-target_lat, target_lon = 50.845, 0.7462166666666667
-current_lat, current_lon = extract_lat_lon(nmea_sentence)
+nmea_sentence = "$MMWPL,5050.710799,N,00044.755897,W,WPT 1*73"
+current_lat, current_lon = 50.845000, -0.746217
+target_lat, target_lon = extract_lat_lon_from_wpl(nmea_sentence)
+print(target_lat,target_lon)
 heading = calculate_heading(target_lat, target_lon, current_lat, current_lon)
 print(f"Heading: {heading:.2f} degrees")
