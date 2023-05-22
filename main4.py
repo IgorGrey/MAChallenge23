1369d2dfd4d2b5693a20b7226c0527aab36d25dd
domdev <drgn.domdev@gmail.com>
2023-05-22 22:36:26 +0100

    Add location to log

--- main4.py
+++ main4.py
@@ -12,24 +12,33 @@ import main1
 SERVER_ADDR = "127.0.0.1"
 SERVER_PORT = 49699
 
+# TODO: create json with config for threshold of pollution
+
 
 # $CCTHD,85.00,0.00,0.00,0.00,0.00,85.00,0.00,0.00
 
 def send_cmd_to_system(cmd):
     _online_port = ports_module.connect_to_port("COM5")
     _online_port.write(cmd)
+    res = _online_port.readline().decode()
+    print(res)
 
-def write_to_log(cmd):
+def write_to_log(sig_cmd, rmc_cmd):
     with open("./challenge4.log", "a+") as file:
         time = datetime.now()
         timestamp = time.strftime("%H:%M:%S")
-        log_entry = f"{timestamp}: {cmd}"
+        log_entry = f"{timestamp}: {rmc_cmd[0]}{rmc_cmd[1]}  {rmc_cmd[2]}{rmc_cmd[3]}  {sig_cmd}%"
         file.write(log_entry + "\n")
 
 def get_pollution_level_from_DYSIG(cmd):
     # print(cmd.split(",")[1].split("*")[0])
     return cmd.split(",")[1].split("*")[0]
 
+
+def get_location_coordinates(cmd):
+    sentence = cmd.split(",")
+    # [3] [4] [5] [6]
+    return [sentence[3], sentence[4], sentence[5], sentence[6]]
 
 def start_search():
     # main1.send_cmd(cmd)
@@ -55,16 +64,19 @@ def start_search():
     print(fwd_sentence)
     send_cmd_to_system(fwd_sentence)
 
-    # hsc_sentence = hsc_sentence + "*" + calculate_checksum(hsc_sentence[1:])
-    # hsc_sentence = hsc_sentence + "\r\n"
-    # hsc_sentence = hsc_sentence.encode("ascii")
+    previous_cmd = ""
 
     while True:
         # Receive data from the server with buffer size
         tcp_data = sock.recv(1024)
         data_decoded = tcp_data.decode('utf-8')
-        pollution_level = get_pollution_level_from_DYSIG(data_decoded)
-        write_to_log(pollution_level)
+        if data_decoded.startswith("$DYSIG"):
+            print(data_decoded)
+            pollution_level = get_pollution_level_from_DYSIG(data_decoded)
+            rmc_coords = get_location_coordinates(previous_cmd)
+            write_to_log(pollution_level, rmc_coords)
+        else:
+            previous_cmd = data_decoded
 
     sock.close()
 
