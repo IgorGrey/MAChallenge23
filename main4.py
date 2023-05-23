80114213e5da1b4d38819f89b46239998f9c09f3
domdev <drgn.domdev@gmail.com>
2023-05-23 22:56:30 +0100

    add plume finding algo v1

--- main4.py
+++ main4.py
@@ -13,6 +13,7 @@ SERVER_ADDR = "127.0.0.1"
 SERVER_PORT = 49699
 
 # TODO: create json with config for threshold of pollution
+THRESHOLD = 12
 
 
 # $CCTHD,85.00,0.00,0.00,0.00,0.00,85.00,0.00,0.00
@@ -40,6 +41,43 @@ def get_location_coordinates(cmd):
     # [3] [4] [5] [6]
     return [sentence[3], sentence[4], sentence[5], sentence[6]]
 
+
+def take_90_degrees_right_turn():
+    # $CCHSC,210.00,T,210.00,M
+    print("Taking turn right")
+    hsc_sentence = generate_thd_hsc.generate_hsc_sentence(210.0)
+    hsc_sentence = hsc_sentence + "*" + main1.calculate_checksum(hsc_sentence[1:])
+    hsc_sentence = hsc_sentence + "\r\n"
+    hsc_sentence = hsc_sentence.encode("ascii")
+    send_cmd_to_system(hsc_sentence)
+
+# This is where we do the Igor's algorithm, we have the info we need at the interval of 1 second
+
+IS_IN_PLUME = False 
+PLUME_ITER = 0
+def algo_challenge4(sig_cmd, rmc_cmd):
+    def set_true_in_plume():
+        is_in_plume = True
+
+    def set_false_in_plume():
+        is_in_plume = False
+
+    print(sig_cmd, rmc_cmd)
+    if IS_IN_PLUME:
+        if sig_cmd < THRESHOLD:
+            print("Left Plume", rmc_cmd[0], rmc_cmd[1], rmc_cmd[2], rmc_cmd[3])
+            IS_IN_PLUME = False
+            if PLUME_ITER > 0:
+                # Take a turn
+                take_90_degrees_right_turn()
+            PLUME_ITER += 1
+
+
+    elif sig_cmd > THRESHOLD:
+        print("Entered plume", rmc_cmd[0], rmc_cmd[1], rmc_cmd[2], rmc_cmd[3])
+        is_in_plume = True
+
+
 def start_search():
     # main1.send_cmd(cmd)
     # main1.calculate_checksum(sentence)
@@ -51,12 +89,15 @@ def start_search():
     print("Starting the challenge")
     print("Setting forward thrust")
     
+    # Turn on the boat
     turn_on_cmd = "$CCAPM,0,64,0,80"
     turn_on_cmd = turn_on_cmd + "*" + main1.calculate_checksum(turn_on_cmd[1:])
     turn_on_cmd = turn_on_cmd + "\r\n"
     turn_on_cmd = turn_on_cmd.encode("ascii")
+    print(turn_on_cmd)
     send_cmd_to_system(turn_on_cmd)
 
+    # Start moving forward 20% thrust
     fwd_sentence = generate_thd_hsc.generate_thd_sentence(20)
     fwd_sentence = fwd_sentence + "*" + main1.calculate_checksum(fwd_sentence[1:])
     fwd_sentence = fwd_sentence + "\r\n"
@@ -71,15 +112,15 @@ def start_search():
         tcp_data = sock.recv(1024)
         data_decoded = tcp_data.decode('utf-8')
         if data_decoded.startswith("$DYSIG"):
-            print(data_decoded)
             pollution_level = get_pollution_level_from_DYSIG(data_decoded)
             rmc_coords = get_location_coordinates(previous_cmd)
+            algo_challenge4(pollution_level, rmc_coords)
             write_to_log(pollution_level, rmc_coords)
+        # get the most recent RMC command
         else:
             previous_cmd = data_decoded
 
     sock.close()
-
 
 
 if __name__ == "__main__":
