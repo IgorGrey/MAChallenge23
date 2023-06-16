a0f93cb5e4fbf2450ef51ac4c37d0045e00d18fb
domdev <drgn.domdev@gmail.com>
2023-06-16 07:08:14 +0100

    Remove offset

--- haversine_formula.py
+++ haversine_formula.py
@@ -1,21 +1,11 @@
 import math
 
-def calulate_new_coords(lat, lon, bearing, distance, offset):
-    lat_deg = int(lat / 100)
-    lat_min = lat % 100
-    lat = lat_deg + lat_min / 60
-
-    lon_deg = int(lon / 100)
-    lon_min = lon % 100
-    lon = lon_deg + lon_min / 60
-
+def calulate_new_coords(lat, lon, bearing, distance):
     lat = math.radians(lat)
     lon = math.radians(lon)
 
     bearing = math.radians(bearing)
     radius = 3440.1
-
-    distance = (distance * 1852 + offset) / (radius * 1852)
 
     new_lat = math.asin(math.sin(lat) * math.cos(distance) +
                         math.cos(lat) * math.sin(distance) * math.cos(bearing))
@@ -25,14 +15,6 @@ def calulate_new_coords(lat, lon, bearing, distance, offset):
 
     new_lat = math.degrees(new_lat)
     new_lon = math.degrees(new_lon)
-
-    new_lat_deg = int(new_lat)
-    new_lat_min = (new_lat - new_lat_deg) * 60
-    new_lat = new_lat_deg * 100 + new_lat_min
-
-    new_lon_deg = int(new_lon)
-    new_lon_min = (new_lon - new_lon_deg) * 60
-    new_lon = new_lon_deg * 100 + new_lon_min
 
     return new_lat, new_lon
 
